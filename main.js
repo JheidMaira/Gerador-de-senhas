4e3476a793f8c2a633ba96495a1e415a18576cde
JheidMaira <143832855+JheidMaira@users.noreply.github.com>
2026-09-15 11:48:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz'
 const numeros = '123456789';
 const simbolos = '!@%*?#';
-const botoes = document.querySelectorAll('parametro-senha__botao');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
-const forcaSenha = document.querySelector('forca');
+const forcaSenha = document.querySelector('.forca');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
@@ -31,3 +31,34 @@ function aumentaTamanho(){
     geraSenha();
 }
 
+for(i=0; i < checkbox.length;i++){
+    checkbox[i].onclick = geraSenha
+}
+
+geraSenha();
+
+function geraSenha(){
+    let alfabeto = '';
+    if(checkbox[0].cheked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].cheked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].cheked){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].cheked){
+        alfabeto = alfabeto + simbolos;
+    }
+
+    let senha = '';
+    for(let i=0;i<tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*alfabeto.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + alfabeto[numeroAleatorio];
+    }
+
+    campoSenha.value = senha;
+    classificaSenha(alfabeto.length);
+}
